f1e3ccba21e51828b663dddb1a1afa581a7f7d92
Near111111 <jomarmerej@gmail.com>
2026-04-25 17:25:09 +0800

    Updated2.0

--- db/database.py
+++ db/database.py
@@ -1,13 +1,20 @@
+# db/database.py
+
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
-MONGODB_URL = os.getenv("MONGODB_URL")
+MONGODB_URI = os.getenv("MONGODB_URI")
 DB_NAME = os.getenv("DB_NAME")
 
-client = AsyncIOMotorClient(MONGODB_URL)
+if not MONGODB_URI:
+    raise ValueError("MONGODB_URI is not set in environment variables.")
+if not DB_NAME:
+    raise ValueError("DB_NAME is not set in environment variables.")
+
+client = AsyncIOMotorClient(MONGODB_URI)
 db = client[DB_NAME]
 
 def get_db():
